525752239bf2c220fad82b92087309628eaf20fd
raidr <decentralizedju@gmail.com>
2023-02-27 17:13:56 -0500

    update

--- components/FetchTokens.tsx
+++ components/FetchTokens.tsx
@@ -4,22 +4,34 @@ import { Connection, GetProgramAccountsFilter } from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { PublicKey } from "@solana/web3.js";
 import { WalletAdapter } from "@solana/wallet-adapter-base";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const fetchTokens = () => {
-  //   const axios = require("axios");
-  //   const api = "4b4253e8-cd33-40e4-b325-4bded4337236";
-  //   let publicKey = useWallet().publicKey;
+  const [solBal, setSolBal] = useState<number>(0);
+  const [tokens, setTokens] = useState<string[]>([]);
+  const axios = require("axios");
+  const api = "4b4253e8-cd33-40e4-b325-4bded4337236";
+  let publicKey = useWallet().publicKey;
 
-  //   const url = `https://api.helius.xyz/v0/addresses/${publicKey}/balances?api-key=${api}`;
-  //   const getBalances = async () => {
-  //     const { data } = await axios.get(url);
-  //     console.log("balances: ", data);
-  //   };
+  const url = `https://api.helius.xyz/v0/addresses/${publicKey}/balances?api-key=${api}`;
+  const getBalances = async () => {
+    const { data } = await axios.get(url);
+    console.log("balances: ", data);
+    setSolBal(data.nativeBalance);
+    // const tokenData = data;
+  };
 
-  //   getBalances();
+  if (useWallet().connected) {
+    getBalances();
+  } else {
+    console.log("connect your wallet!");
+  }
 
-  return <div>fetchTokens</div>;
+  return (
+    <div>
+      <div>SOL Balance: {solBal / 1000000000}</div>
+    </div>
+  );
 };
 
 export default fetchTokens;
