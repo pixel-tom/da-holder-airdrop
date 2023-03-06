a4f85914ea04b3e2557a77be3a95ea7f99961c95
raidr <decentralizedju@gmail.com>
2023-03-06 12:30:31 -0500

    update

--- pages/tokens.tsx
+++ pages/tokens.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import FetchTokens from "../components/FetchTokens";
 import WalletButton from "../components/nfts/FindAllByOwner";
 import HeliusMintlist from "../components/mintlist/HeliusMintlist";
+import AirdropSol from "../components/airdrop/AirdropSol";
 
 const tokens = () => {
+  const [recipientAddresses, setRecipientAddresses] = useState<string[]>([]);
+
   return (
     <div>
       {" "}
@@ -22,6 +26,7 @@ const tokens = () => {
       <div>
         <WalletButton />
         <HeliusMintlist />
+        {/* <AirdropSol recipientAddresses={recipientAddresses} /> */}
       </div>
     </div>
   );
