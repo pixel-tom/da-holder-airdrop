84de1463e03e2ffd9da4636ea684210557ec2eb5
raidr <decentralizedju@gmail.com>
2023-03-10 14:01:24 -0500

    added txn fee

--- components/airdrop/AirdropTest.tsx
+++ components/airdrop/AirdropTest.tsx
@@ -6,7 +6,12 @@ import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
   createTransferInstruction,
 } from "@solana/spl-token";
-import { PublicKey, Connection, Transaction } from "@solana/web3.js";
+import {
+  PublicKey,
+  Connection,
+  Transaction,
+  SystemProgram,
+} from "@solana/web3.js";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { toast } from "react-toastify";
 
@@ -18,8 +23,8 @@ const AirdropTest = ({
   const connection = new Connection("https://api.devnet.solana.com");
   const { publicKey, signTransaction } = useWallet();
   const mintKeys = [
-    new PublicKey("4XDirEKVBVAFvTCMXjoiH4sURDzLyvxYs2tL9jkNdHEL"),
-    new PublicKey("AMW5AYYKbbpcnweH4e2SJARiWrGCeMKdPsJieUeC3Mco"),
+    new PublicKey("2gVwm5j1Dy6u88tmJumVpVmdEa9vGzWGCiqThazHCaj6"),
+    new PublicKey("Dbr6ZzHhiG9yA4nQP1oe4qyzbXF8utVDf9veG1WoDCDD"),
   ];
 
   let testMintKey = new PublicKey(
@@ -27,8 +32,8 @@ const AirdropTest = ({
   );
 
   let testHolders = [
-    "HkTFX8Vk22ZcMN2G5MK5g4jbnLVZ41f77qTYXtcjYG3X",
-    "DWWbMxqKXc5Ban4maY2SokzvcQUTZp4UWM2GPWF1fb9E",
+    "EJ9vJt8pr4RKptxCxb1TrdbFjFzbTdkx3hpqvs3a2NDL",
+    "7LqBQnMxcyZyzNwgA75cBm6TA7TsALbSKJpVZtGiyhEG",
   ];
 
   // when not testing return value back to <recipientAddresses> below
@@ -85,6 +90,27 @@ const AirdropTest = ({
         )
       );
     }
+
+    // the const below is to figure out how many NFTs are being sent so we can calculate the txn fee we want to implement
+
+    // 3000000 lamports = .003 SOL
+    const txnFee = holders.length * 3000000;
+
+    // Also have to create a treasury to store accumulated fees
+
+    const treasury = new PublicKey(
+      "8uToe5ptfG8VcQjAbr3FFkPmtRysiUv8ABcbpxyDnfYt"
+    );
+
+    // Add a transfer instruction to the transaction
+    tx.add(
+      // The transfer instruction transfers .003 SOL from the sender to the treasury
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: treasury,
+        lamports: txnFee, // 1 SOL = 1 billion lamports
+      })
+    );
 
     tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
     tx.feePayer = publicKey;
