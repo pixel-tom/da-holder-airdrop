9ce407fbc80b567f5b4b4b38179d77ec1df103ba
raidr <decentralizedju@gmail.com>
2023-03-13 16:32:47 -0400

    created droplist for nfts

--- utils/dropList.ts
+++ utils/dropList.ts
@@ -36,3 +36,42 @@ export const dropList: Drop[] = [
     numLamports: 1000000000,
   },
 ];
+
+export interface DropNft {
+  walletAddress: string;
+  numTokens: number;
+}
+export const dropListNft: DropNft[] = [
+  {
+    walletAddress: "EJ9vJt8pr4RKptxCxb1TrdbFjFzbTdkx3hpqvs3a2NDL",
+    numTokens: 1,
+  },
+  {
+    walletAddress: "7LqBQnMxcyZyzNwgA75cBm6TA7TsALbSKJpVZtGiyhEG",
+    numTokens: 1,
+  },
+  {
+    walletAddress: "C7LqZMecK7GzTPVFeDyzWKbshDBkBjbNWWfvUatEZMq5",
+    numTokens: 1,
+  },
+  {
+    walletAddress: "DJ1Gj18EsiFwp1xZDTg45MMjWTM5ShVi5oS4KXU9dKHa",
+    numTokens: 1,
+  },
+  {
+    walletAddress: "CgDhA9sVtQFuFc5eKp3YjdxfWNYpymBdGGzeobfczG2d",
+    numTokens: 1,
+  },
+  {
+    walletAddress: "BWW3YpVymiyxyVuQoJLjtNisYer2EJefsuKb648g2u8f",
+    numTokens: 1,
+  },
+  {
+    walletAddress: "9LHVXpgKQKvApb5v4VJZMBYaTJ3AbzRFTgRH8nmjjzJq",
+    numTokens: 1,
+  },
+  {
+    walletAddress: "C9Q3tw8sSwTEPLCpRMtGgbUPM72TPTNzmJu7Kvy6Acxb",
+    numTokens: 1,
+  },
+];
